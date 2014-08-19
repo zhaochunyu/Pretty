026ac2bb713ab842a1292ea3eddec65ee49e2a45
chunchun <chunchun@chunchun-V>
2014-08-19 10:58:11 +0800

    修改读取日志方式

--- models/db.js
+++ models/db.js
@@ -6,9 +6,9 @@ var pool = poolModule.Pool({
     name : 'mongodb',
     create : function(callback) {
 //    	mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
-        mongodb.MongoClient.connect('mongodb://'+settings.username+':'+settings.password+'@'+settings.host+':27017/'+settings.db, {
+//        mongodb.MongoClient.connect('mongodb://'+settings.username+':'+settings.password+'@'+settings.host+':27017/'+settings.db, {
 
-//    	mongodb.MongoClient.connect('mongodb://'+settings.host+':27017/'+settings.db, {
+    	mongodb.MongoClient.connect('mongodb://'+settings.host+':27017/'+settings.db, {
             server:{poolSize:1}
         }, function(err, db){
             callback(err, db);
--- models/readlog.js
+++ models/readlog.js
@@ -1,19 +1,44 @@
 var Connection = require('ssh2');
 var iconv = require('iconv-lite');
 var logger = require('../log4js').logger;
+var spawn = require('child_process').spawn;
 function Readlog() {
 }
 module.exports = Readlog;
 
-// 查看各服务器启动日志
+//查看各服务器启动日志
 Readlog.read = function(logname,callback) {
+	var datar='';
+	logger.info("开始读取日志:"+'nl /root/shng/qalog/'+logname);
+	ready = spawn('nl', ['/root/shng/qalog/'+logname]);
+	ready.stdout.on('data', function (data) {
+		  datar = datar+iconv.decode(data, 'GBK');//return GBK encoded bytes from unicode string
+			 datar= datar.toString();		 
+			});		
+	ready.stderr.on('data', function (data) {
+		logger.error('执行失败：'+data);
+	return callback('null');
+});
+	ready.stdout.on('end', function (data) {
+		return callback(null, datar);
+}
+	);
+	ready.on('close', function (code) {
+		logger.info('child process exited with code ' + code);
+		});	
+	
+}
+
+
+// 查看各服务器启动日志
+Readlog.readold = function(logname,callback) {
 	var datar='';
 	var c = new Connection();
 	c.connect({
 		host : '172.17.103.6',
 		port : 22,
 		username : 'root',
-		password : '9o3kzlQA',
+		password : '****',
 		publicKey: require('fs').readFileSync('/root/.ssh/id_dsa.pub')
 	});
 	c.on('ready', function() {		
